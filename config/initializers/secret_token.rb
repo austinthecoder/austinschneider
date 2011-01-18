9e9811c33bc6941b15364718305dfeef0bd19b07
Austin Schneider <soccer022483@gmail.com>
2011-01-17 20:56:12 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Austinschneider::Application.config.secret_token = '23fe1fc7a34531608cdcefe75a9f71df6adf6b6411dd0f20223c89474f3e0e9fdbcc7b7188e00400722905035b0a2593a45c77c48786e77e2938844b4e20384f'
