fd19be78f5dabd01ada1e64b2713faca3925c784
Austin Schneider <austinthecoder@gmail.com>
2012-10-22 19:12:14 -0400

    raise error to test crashlog

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,8 @@ class PagesController < ApplicationController
 
   caches_page :home
 
+  def error
+    raise StandardError, "Ahhh! You found me!"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ AustinSchneider::Application.routes.draw do
 
   root :to => 'pages#home'
 
+  get 'error', :to => 'pages#error'
+
 end
