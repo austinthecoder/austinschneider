58c2fb07e7743eb492a5b7286ae6df6bd7dfc02f
Austin Schneider <austinthecoder@gmail.com>
2013-06-07 18:06:18 -0400

    remove redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,3 @@
 class ApplicationController < ActionController::Base
-
-  before_filter :ensure_no_subdomains
-
   protect_from_forgery
-
-private
-
-  def ensure_no_subdomains
-    if request.subdomains.present? && request.domain == 'austinschneider.com'
-      redirect_to root_url(:host => request.domain, :port => request.port)
-    end
-  end
-
 end
